641e0e7b83fadf33bb8868f5fee12badbe0ac799
ian monnox <tc.info@tradecontrol.co.uk>
2020-10-01 18:47:30 +0100

    3.30.3 fix

--- src/tcNode/scripts/tc_upgrade_3_30_3.sql
+++ src/tcNode/scripts/tc_upgrade_3_30_3.sql
@@ -657,6 +657,28 @@ AS
 	FROM vat_statement
 	WHERE StartOn >= (SELECT MIN(StartOn) FROM App.tbYearPeriod p JOIN App.tbYear y ON p.YearNumber = y.YearNumber  WHERE y.CashStatusCode < 3);
 go
+ALTER VIEW Cash.vwBalanceSheetVat
+AS
+	WITH asset_rows AS
+	(
+		SELECT StartOn, MAX(RowNumber) AssetRow
+		FROM Cash.vwTaxVatStatement
+		GROUP BY StartOn
+	)
+	SELECT tax_type.AssetCode, tax_type.AssetName, 
+		CAST(0 as smallint) CashModeCode,  
+		CAST(1 as smallint) AssetTypeCode,  
+		DATEADD(MONTH, 1, DATEADD(DAY, (DATEPART(DAY, vat.StartOn) * -1) + 1, vat.StartOn)) StartOn, 		
+		Balance * -1 Balance 
+	FROM Cash.vwTaxVatStatement vat
+		JOIN asset_rows ON vat.RowNumber = asset_rows.AssetRow
+		CROSS JOIN
+		(
+			SELECT UPPER(LEFT(TaxType, 3)) AssetCode, UPPER(TaxType) AssetName
+			FROM Cash.tbTaxType
+			WHERE TaxTypeCode = 1
+		) tax_type;
+go
 
 
 
